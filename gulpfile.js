5f2802205bd7e6e6a793fdb27da42d2b40ae8212
yujuiting <yujuiting@me.com>
2016-07-28 06:25:49 +0800

    build task and update ignore file

--- gulpfile.js
+++ gulpfile.js
@@ -2,65 +2,48 @@ const gulp = require('gulp');
 const run = require('run-sequence');
 const del = require('del');
 const inlineNg2Template = require('gulp-inline-ng2-template');
-const fs = require('fs');
 
-const vendor = [
-  './node_modules/@angular/**/*.js',
-  './node_modules/zone.js/dist/*.js',
-  './node_modules/es6-shim/es6-shim.js',
-  './node_modules/reflect-metadata/*.js',
-  './node_modules/bootstrap/dist/**/*.*',
-  './node_modules/prismjs/themes/**/*.css',
-  './node_modules/prismjs/prism.js',
-  './node_modules/rxjs/**/*.js',
-  './node_modules/systemjs/dist/system.js',
-  './node_modules/symbol-observable/**/*.js'
-];
-
-gulp.task('build:example', () => {
-  return run(
-    ['clear:example-vendor', 'clear:example-dist'],
-    ['build:example-vendor', 'build:example-files']
-  );
-});
-
-gulp.task('clear:example-dist', () => {
-  return del('./example/dist/');
-});
-
-gulp.task('clear:example-vendor', () => {
-  return del('./example/vendor/');
-});
-
-gulp.task('build:example-vendor', () => {
-  gulp.src(vendor, { base: './node_modules' })
-    .pipe(gulp.dest('./example/vendor'));
-});
-
-gulp.task('build:example-files', () => {
-  gulp.src('./example/src/**/*.{html,css}', { base: './example/src' })
-    .pipe(gulp.dest('./example/dist'));
-});
-
-gulp.task('watch:example-files', () => {
-  gulp.watch('./example/src/**/*.{html,css}', ['build:example-files'])
-});
-
-gulp.task('build:lib:inline-template', () => {
-  gulp.src('./lib/**/*.js', { base: './lib' })
-    .pipe(inlineNg2Template({ base: './src/lib' }))
-    .pipe(gulp.dest('./lib'));
+gulp.task('build:core:inline-template', () => {
+  gulp.src('./src/**/*.js', { base: './src' })
+    .pipe(inlineNg2Template({ useRelativePaths: true }))
+    .pipe(gulp.dest('./src'));
 });
 
 gulp.task('build:components:inline-template', () => {
-  gulp.src('./components/**/*.js', { base: './components' })
-    .pipe(inlineNg2Template({ base: './src/components' }))
-    .pipe(gulp.dest('./components'));
+  gulp.src('./src/components/**/*.js', { base: './src/components' })
+    .pipe(inlineNg2Template({ useRelativePaths: true }))
+    .pipe(gulp.dest('./src/components'));
 });
 
 gulp.task('build:inline-template', () => {
   return run([
-    'build:lib:inline-template',
+    'build:core:inline-template',
     'build:components:inline-template'
   ])
 });
+
+gulp.task('clear', () => 
+  del([
+    './components',
+    './core',
+    './index.{js,js.map,d.ts}',
+    './components.{js,js.map,d.ts}'
+  ]));
+
+gulp.task('build:move', () =>
+  gulp.src('./src/**/*.{js,js.map,d.ts}', { base: './src' })
+    .pipe(gulp.dest('./')));
+
+gulp.task('build:clear', () =>
+  del([
+    './src/**/*.{js,js.map,d.ts}',
+    '!./src/typings.d.ts'
+  ]));
+
+gulp.task('after-build', (done) =>
+  run(
+    'build:inline-template',
+    'build:move',
+    'build:clear',
+    done
+  ));
