cef2130e504651157f5a4fc67a77bb3fbf6dbe67
christopher_yu <christopher_yu@everfocus.com.tw>
2016-10-24 10:28:55 +0800

    example env

--- example/main.ts
+++ example/main.ts
@@ -1,5 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/';
+import { environment } from './environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
