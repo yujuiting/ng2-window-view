568e3c6ae75deaa6379c36b39f345d414883b089
christopher_yu <christopher_yu@everfocus.com.tw>
2016-10-26 12:01:40 +0800

    cleanup

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/prism/index.d.ts" />
-/// <reference path="globals/require/index.d.ts" />
-/// <reference path="globals/source-map/index.d.ts" />
-/// <reference path="globals/uglify-js/index.d.ts" />
-/// <reference path="globals/webpack/index.d.ts" />
